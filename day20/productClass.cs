2d6b36dbcf4fa53c4e95e51781934d7f662bf17d
gagan <96785457+gagan20003@users.noreply.github.com>
2025-11-12 09:48:44 +0530

    Create productClass.cs

---
+++ day20/productClass.cs
@@ -0,0 +1,10 @@
+namespace ProductsApplication.Models
+{
+    public class ProductsClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+    }
+}
